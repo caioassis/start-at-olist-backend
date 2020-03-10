e1d0131fab780d8220bad64c08d635300140ccd4
Caio Eduardo Borges Assis <caioassis@hotmail.com>
2020-03-10 16:19:56 -0300

    Define url names

--- records/urls.py
+++ records/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CallEndRecordAPIView, CallStartRecordAPIView, TelephonyBillAPIView
 
 urlpatterns = [
-    path('started/', CallStartRecordAPIView.as_view()),
-    path('finished/', CallEndRecordAPIView.as_view()),
-    path('bills/', TelephonyBillAPIView.as_view())
+    path('started/', CallStartRecordAPIView.as_view(), name='call_start_record_create'),
+    path('finished/', CallEndRecordAPIView.as_view(), name='call_end_record_create'),
+    path('bills/', TelephonyBillAPIView.as_view(), name='get_telephony_bill')
 ]
