fe90eb7b2d1758bb47c909f4f72e7c0150eca490
Caio Eduardo Borges Assis <caioassis@hotmail.com>
2020-03-13 11:22:44 -0300

    Rest framework settings configuration

--- telecom/settings.py
+++ telecom/settings.py
@@ -84,3 +84,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
+}
