fa03e6a708005f44e6be62e609dd4f9caad822ad
Caio Eduardo Borges Assis <caioassis@hotmail.com>
2020-03-20 13:12:17 -0300

    Remove admin file and url

--- telecom/urls.py
+++ telecom/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('call_records/', include('records.urls')),
     path('docs/', include_docs_urls(title='Library API Project'))
 ]
